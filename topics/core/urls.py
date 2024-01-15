0f4b8b64d33ed0e4ee767a44d17fc8705244fc45
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-01-15 04:10:30 -0600

    Route debugtoolbar

--- topics/core/urls.py
+++ topics/core/urls.py
@@ -1,6 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("__debug__/", include("debug_toolbar.urls")),
     path("admin/", admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
