0a027ab84da216e87963ce87a81b8f84536cc1c3
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-01-18 20:24:04 -0600

    Add discussion and comment views; implement pagination

--- topics/content/urls.py
+++ topics/content/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path("", views.topic_list, name="topic_list"),
     path("t/<int:pk>/", views.topic_detail, name="topic_detail"),
     path("d/<int:pk>/", views.discussion_detail, name="discussion_detail"),
+    path("u/<int:pk>/", views.user_detail, name="user_detail"),
+    path("u/<int:pk>/comments/", views.user_comments, name="user_comments"),
+    path("u/<int:pk>/discussions/", views.user_discussions, name="user_discussions"),
 ]
--- topics/content/views.py
+++ topics/content/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.models import User
+from django.core.paginator import Paginator
 from django.template.response import TemplateResponse
 
-from content.models import Category, Topic, Discussion
+from content.models import Category, Topic, Discussion, Comment
 
 
 def topic_list(request):
@@ -24,10 +26,57 @@ def topic_detail(request, pk):
 
 
 def discussion_detail(request, pk):
+    comments = Comment.objects.filter(discussion_id=pk)
+    paginator = Paginator(comments, 50)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
     return TemplateResponse(
         request=request,
         template="content/discussion_detail.html",
         context={
             "discussion": Discussion.objects.get(pk=pk),
+            "page_obj": page_obj,
+        },
+    )
+
+
+def user_detail(request, pk):
+    return TemplateResponse(
+        request=request,
+        template="content/user_detail.html",
+        context={
+            "user": User.objects.get(pk=pk),
+            "discussions": Discussion.objects.filter(author_id=pk)[:5],
+            "comments": Comment.objects.filter(author_id=pk)[:5],
+        },
+    )
+
+
+def user_comments(request, pk):
+    comments = Comment.objects.filter(author_id=pk)
+    paginator = Paginator(comments, 50)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    return TemplateResponse(
+        request=request,
+        template="content/user_comments.html",
+        context={
+            "user": User.objects.get(pk=pk),
+            "page_obj": page_obj,
+        },
+    )
+
+
+def user_discussions(request, pk):
+    discussions = Discussion.objects.filter(author_id=pk)
+    paginator = Paginator(discussions, 25)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    return TemplateResponse(
+        request=request,
+        template="content/user_discussions.html",
+        context={
+            "user": User.objects.get(pk=pk),
+            "page_obj": page_obj,
         },
     )
